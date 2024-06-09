e0855328f1e188de321c99c181925092b42daaec
unknown <rishisutar183@gmail.com>
2024-06-09 14:10:26 +0530

    Projects

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from PIL import Image
+from io import BytesIO
+from fastai.vision.all import *
+import pathlib
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+learner = load_learner('model/real_ai.pkl')
+
+# Function to check if image is AI-generated or real
+
+
+def check_image(image):
+
+    img = image.resize((460, 460))
+    ans = learner.predict(img)
+    return ans[0]
+
+# Streamlit UI
+def main():
+    st.title("AI-Generated Image Detector")
+    st.write("Upload an image to check if it's AI-generated or real")
+
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+    if uploaded_file is not None:
+        image = PILImage.create(uploaded_file)
+        width, height = image.size
+        
+        # Check if the dimensions are smaller than the threshold
+        if width < 100 or height < 100:
+            st.write(f"Image Size is too small")
+        else:
+            st.image(image, caption='Uploaded Image.', use_column_width=True)
+            st.write("")
+            st.write("Classifying...")
+            result = check_image(image)
+            st.write(f"The uploaded image is: {result}")
+
+if __name__ == '__main__':
+    main()
